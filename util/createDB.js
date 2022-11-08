ee9e1a9810252bc802f991b5099bb6bb8e6e6a86
Pin Shan Chuang <pshanchuang@gmail.com>
2022-11-08 14:52:46 +0800

    chores: 修改 createDB 腳本

--- util/createDB.js
+++ util/createDB.js
@@ -2,8 +2,11 @@ require('dotenv').config({ path: '../.env' })
 const db = require('../models/mysqlconf')
 
 const createTable = async () => {
-  const [result] = await db.query('select * from product')
-  console.log(result)
+  const [result] = await db.query(
+    `INSERT INTO messages (send_user_id, content, created_at, room_id) VALUES (5, 'gogogo!', convert_tz(UTC_TIMESTAMP(),'+00:00','+08:00'), 4567)`
+  )
+
+  console.log(result.insertId)
 }
 
 createTable()
