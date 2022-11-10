29ae8ce2c10f7bfae5be66172705656d7a00e6cb
Pin Shan Chuang <pshanchuang@gmail.com>
2022-11-10 13:50:01 +0800

    chores: 修改 createDB 腳本

--- util/createDB.js
+++ util/createDB.js
@@ -1,12 +1,47 @@
 require('dotenv').config({ path: '../.env' })
+const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env
+
 const db = require('../models/mysqlconf')
 
-const createTable = async () => {
-  const [result] = await db.query(
-    `INSERT INTO messages (send_user_id, content, created_at, room_id) VALUES (5, 'gogogo!', convert_tz(UTC_TIMESTAMP(),'+00:00','+08:00'), 4567)`
-  )
+// const mysql = require('mysql2/promise')
+// const db = mysql.createPool({
+//   host: DB_HOST,
+//   user: DB_USERNAME,
+//   password: DB_PASSWORD,
+//   database: 'stylish_backend',
+// })
 
-  console.log(result.insertId)
+const getTime = async () => {
+  const [times] = await db.query(`select * from time`)
+  console.log(times)
+
+  const time = times[0].time
+  console.log('time', time)
+  const datetime = new Date(times[0].datetime)
+  const datetime2 = new Date(times[1].datetime)
+  console.log('datetime: ', datetime)
+  console.log('datetime: ', datetime.toLocaleString())
+  console.log('datetime2: ', datetime2)
+  console.log('datetime2: ', datetime2.toLocaleString())
+
+  console.log('datetime - time in mins: ', (datetime - time) / 60000)
+  console.log('datetime2 - datetime in mins: ', (datetime2 - datetime) / 60000)
 }
 
-createTable()
+const insertTime = async () => {
+  const datetime = Date.now()
+  await db.query(`UPDATE time SET datetime = ? WHERE id = ?`, [datetime, 16])
+}
+
+const getDateTime = async () => {
+  const [reply] = await db.query('SELECT * FROM replies WHERE id = 14')
+  const time = reply[0].time
+  console.log(reply)
+  console.log(time)
+
+  console.log(new Date(time).toLocaleString())
+}
+
+// insertTime()
+// getTime()
+getDateTime()
