870a2e65984559cd3d811b58ff58febc94d05244
Pin Shan Chuang <pshanchuang@gmail.com>
2022-11-18 13:50:39 +0800

    fix: 將 socket cors origin 設為全開

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const Chatroom = require('./models/chatroom_model')
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST'],
   },
 })
