e8a759f5a4b5549ad77d0c8ed3042e2c9e66260b
Pin Shan Chuang <pshanchuang@gmail.com>
2022-11-19 20:06:46 +0800

    feat: 時間相關判斷移動去 redis (rooms & match-list & datetime)

--- server.js
+++ server.js
@@ -6,8 +6,15 @@ const server = http.createServer(app)
 
 const { PORT, TOKEN_SECRET } = process.env
 const { v4: uuidv4 } = require('uuid')
+const redis = require('./util/cache')
 const jwt = require('jsonwebtoken')
+const dayjs = require('dayjs')
+var utc = require('dayjs/plugin/utc')
+dayjs.extend(utc)
+
 const Chatroom = require('./models/chatroom_model')
+const Questions = require('./models/questions_model')
+const EXPIRE_TIME = 30 // match 和 room 的 redis key expire 時間先設定 30 秒 之後要改成 24 hr
 
 const io = new Server(server, {
   cors: {
@@ -16,8 +23,6 @@ const io = new Server(server, {
   },
 })
 
-// rooms
-let rooms = {}
 let users = {}
 // io.use((socket, next) => {
 //   console.log('the error middleware')
@@ -54,23 +59,30 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('Got disconnected')
     delete users[socket.user.id]
-    console.log('users:', users)
-    // TODO: handle rooms 要把已經不在的 room 刪掉
-    console.log('rooms: ', rooms)
+    // console.log('users:', users)
   })
 
   // 收到訊息後轉發同個聊天室
-  socket.on('send-message', (msg) => {
+  socket.on('send-message', async (msg) => {
     // msg: {roomId: xxx, message: 'hello', userId: 13}
     console.log('msg:', msg)
 
     if (!msg.roomId) {
-      console.log('no counterpart selected!')
+      socket.emit('room-not-exist', { message: 'room not exist' })
       return
     }
 
     // 1. 自己管 room
-    let members = rooms[msg.roomId].members
+    const result = await redis.hget('room:' + msg.roomId, 'members')
+
+    const members = JSON.parse(result)
+    if (!members) {
+      socket.emit('room-not-exist', { message: 'room not exist' })
+      return
+    }
+
+    console.log('redis members', members)
+    // let members = rooms[msg.roomId].members
     console.log('members: ', members)
     if (members[0] == socket.user.id) {
       users[members[1]].emit('receive-message', {
@@ -102,13 +114,25 @@ io.on('connection', (socket) => {
   })
 
   // client 端點擊 repliers 發送 create room 事件後
-  socket.on('create-room', (data) => {
+  socket.on('create-room', async (data) => {
     console.log('data: ', data)
-    // data: {'counterpart': 2}
+    // data: {'counterpart': 2, questionId: 52}
     // 如果前端選的 user 目前不在線上，則不能建立聊天室
     if (!users[data.counterpart]) {
       socket.emit('create-room-fail', {
         message: `could not find ${data.counterpart}`,
+      })
+      return
+    }
+
+    // questions table 的 is_closed 改成 1
+    await Questions.closeQuestion(parseInt(data.questionId))
+
+    // 判斷選擇的人是不是已經配對過
+    const matchList = await redis.lrange('match-list', 0, -1)
+    if (matchList.includes(data.counterpart.toString())) {
+      socket.emit('create-room-fail', {
+        message: `user ${data.counterpart} has been matched.`,
       })
       return
     }
@@ -120,16 +144,20 @@ io.on('connection', (socket) => {
     //   'created_at' : '2022/11/4 13:30'
     // }
 
-    rooms[roomId] = {
-      members: [parseInt(socket.user.id), data.counterpart],
-      created_dt: Date.now(),
+    // 紀錄配對聊天室 roomId, 雙方 user_id, 開始聊天時間
+    const roomData = {
+      members: JSON.stringify([socket.user.id, data.counterpart]),
+      created_dt: dayjs().utc().format('YYYY-MM-DD HH:mm:ss'),
     }
 
-    console.log('rooms: ', rooms)
+    await redis.hmset('room:' + roomId, roomData)
+    await redis.expire('room:' + roomId, EXPIRE_TIME) // 設定
 
-    // 2. 讓 socket.io 管
-    // socket.join(roomId)
-    // users[data.couterpart].join(roomId)
+    console.log('roomData: ', roomData)
+
+    // 紀錄已配對成功的人
+    await redis.lpush('match-list', [socket.user.id, data.counterpart])
+    await redis.expire('match-list', EXPIRE_TIME) // 設定
 
     socket.emit('create-room-ok', { roomId, counterpart: data.counterpart }) // 回傳給自己 roomId 以及聊天對象 id
 
@@ -139,6 +167,12 @@ io.on('connection', (socket) => {
       counterpart: parseInt(socket.user.id),
       isPassive: true, // 被選中的人是多回傳 isPassive: true
     })
+
+    // TODO: 15 分鐘後結束聊天室
+    // setTimeout(() => {
+    //   socket.emit('match-time-end')
+    //   users[data.counterpart].emit('match-time-end')
+    // }, 7000)
   })
 
   // client 端點擊好友發送 join-room 事件
