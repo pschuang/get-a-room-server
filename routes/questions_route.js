55f0145e47ca1ad2382395938dca32422e14e8b9
Pin Shan Chuang <pshanchuang@gmail.com>
2022-11-19 19:58:32 +0800

    fix: questions route 加上 時間判斷 middleware

--- routes/questions_route.js
+++ routes/questions_route.js
@@ -10,12 +10,14 @@ const {
   createReply,
 } = require('../controllers/questions_controller')
 
-router.route('/questions/status').get(authentication, checkStatus)
+router
+  .route('/questions/status')
+  .get(authentication, isBulletinOpen, checkStatus)
 router.route('/questions/:category').get(isBulletinOpen, getQuestions)
 router
   .route('/questions/details/:questionId')
-  .get(authentication, getQuestionsDetails)
-router.route('/questions').post(authentication, createQuestion)
-router.route('/reply').post(authentication, createReply)
+  .get(authentication, isBulletinOpen, getQuestionsDetails)
+router.route('/questions').post(authentication, isBulletinOpen, createQuestion)
+router.route('/reply').post(authentication, isBulletinOpen, createReply)
 
 module.exports = router
